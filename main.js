e7fa60c4fcd9b82884a0caee0b4a0e081ada2a27
omar6685 <omr6685@hotmail.com>
2023-06-19 11:05:34 +0300

    second edit of about

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const nextBtns = document.querySelectorAll(".n-x");
 const progress = document.getElementById("progress");
 const formSteps = document.querySelectorAll(".form-step");
 const progressSteps = document.querySelectorAll(".progress-step");
+const body = document.body;
 
 let formStepsNum = 0;
-
 nextBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
         formStepsNum++;
         updateFormSteps();
         updateProgressbar();
+        updatePageHeight();
+        ddd(); // Call the ddd function
     });
 });
 
@@ -19,16 +21,27 @@ prevBtns.forEach((btn) => {
         formStepsNum--;
         updateFormSteps();
         updateProgressbar();
+        updatePageHeight();
+        ddd(); // Call the ddd function
     });
 });
 
 function updateFormSteps() {
     formSteps.forEach((formStep) => {
-        formStep.classList.contains("form-step-active") &&
-            formStep.classList.remove("form-step-active");
+        formStep.classList.remove("form-step-active");
     });
 
     formSteps[formStepsNum].classList.add("form-step-active");
+}
+
+function updatePageHeight() {
+    if (formStepsNum === 0) {
+        body.classList.remove("bg-im-2");
+        body.classList.add("bg-im");
+    } else {
+        body.classList.remove("bg-im");
+        body.classList.add("bg-im-2");
+    }
 }
 
 function updateProgressbar() {
@@ -45,3 +58,10 @@ function updateProgressbar() {
     progress.style.width =
         ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
 }
+const ddd = () => {
+    if (body.classList.contains("bg-im")) {
+        console.log("aaaa")
+    }
+
+}
+
