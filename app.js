0fd45bf6ba9852b3f56cdb127682cc879c45bf8f
omar6685 <omr6685@hotmail.com>
2023-07-18 21:32:50 +0300

    edit our clients

--- app.js
+++ app.js
@@ -11,23 +11,6 @@ window.addEventListener('scroll', () => {
     // Apply the rotation to the main image
     mainImage.style.transform = `rotate(${rotation}deg)`;
     
-// Get the position and dimensions of the "clients-5" element
-let clients5 = document.querySelector(".clients-6");
-let clients5Rect = clients5.getBoundingClientRect();
-let footer = document.querySelectorAll("footer")
-
-// Calculate the scroll position at which the "clients-5" element should start moving
-let startScroll = (clients5Rect.top) - window.innerHeight;
-
-// Calculate the scroll position at which the "clients-5" element should stop moving
-let stopScroll = clients5Rect.bottom+1200;
-
-// Change the "margin-left" of the "clients-5" element based on the scroll position
-if (scroll >= startScroll && scroll <= stopScroll) {
-    let progress = (scroll - startScroll) / (stopScroll - startScroll);
-    let marginLeft = 50 - progress * 104;
-    clients5.style.marginLeft = `${marginLeft}%`;
-}
 
 });
 
