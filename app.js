615be296ee01dc9e6e53efa9e3eedac4f5b36b31
omar6685 <omr6685@hotmail.com>
2023-08-19 17:32:54 +0300

    add packages js

--- app.js
+++ app.js
@@ -15,3 +15,18 @@ window.addEventListener('scroll', () => {
 });
 
 // Add the following code inside the window.addEventListener('scroll', () => {}) block
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
